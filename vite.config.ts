66f9e16fa545c51e0fd876e48243964b9354fb5b
harry <bardenha@gmail.com>
2023-08-05 11:04:48 +0100

    feat: add bundle analyser

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,27 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
+export default defineConfig(() => {
+  const env = loadEnv('all', process.cwd(), '');
+
+  return {
+    plugins: [
+      solidPlugin(),
+      visualizer({
+        open: env.VITE_OPEN_VISUALIZER === 'true',
+      }),
+    ],
+    server: {
+      port: 3000,
     },
-  },
+    build: {
+      target: 'esnext',
+    },
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
+  };
 });
