2a8fc83db123fe320dc01dcf4f181ad310c84e53
harry <bardenha@gmail.com>
2023-08-05 11:08:43 +0100

    feat: bundle compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import { visualizer } from 'rollup-plugin-visualizer';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig(() => {
   const env = loadEnv('all', process.cwd(), '');
@@ -8,6 +9,13 @@ export default defineConfig(() => {
   return {
     plugins: [
       solidPlugin(),
+      viteCompression({
+        verbose: true,
+        disable: false,
+        threshold: 10240,
+        algorithm: 'gzip',
+        ext: '.gz',
+      }),
       visualizer({
         open: env.VITE_OPEN_VISUALIZER === 'true',
       }),
