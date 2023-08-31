62ca84fcee237b797d5f64479e58a9e140fba75a
Harry <bardenha@gmail.com>
2023-08-31 23:06:17 +0100

    chore: formatting

--- src/lib/components/Typography/Code.tsx
+++ src/lib/components/Typography/Code.tsx
@@ -5,19 +5,19 @@ export function Code(props: Readonly<JSX.HTMLAttributes<HTMLElement>>): JSX.Elem
     <Show
       when={props.class?.includes('language-')}
       fallback={
-        <code {...props} class="rounded bg-element px-1 py-1">
+        <code {...props} class="bg-element rounded px-1 py-1">
           {props.children}
         </code>
       }
     >
       <div class="group relative">
-        <button class="copy-code absolute right-2 top-2 z-10 hidden rounded-sm bg-elevate px-2 py-1 text-xs font-medium group-hover:block">
+        <button class="copy-code bg-elevate absolute right-2 top-2 z-10 hidden rounded-sm px-2 py-1 text-xs font-medium group-hover:block">
           Copy
         </button>
 
         <code
           {...props}
-          class="my-7 grid overflow-x-scroll rounded border border-elevate bg-neutral p-5 pr-6 text-sm font-medium leading-normal"
+          class="border-elevate bg-neutral my-7 grid overflow-x-scroll rounded border p-5 pr-6 text-sm font-medium leading-normal"
         >
           {props.children}
         </code>
