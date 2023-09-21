3262e7bd96b555a65fc62c16fdfc2d419210e43e
harry <bardenha@gmail.com>
2023-09-21 21:05:22 +0100

    feat: add project links

--- src/views/project-card.tsx
+++ src/views/project-card.tsx
@@ -1,7 +1,7 @@
 import { CollectionEntry } from 'astro:content';
 import { JSX, Match, Show, splitProps, Switch } from 'solid-js';
 
-import { Card, Typography } from '@/lib/components';
+import { Anchor, Card, Typography } from '@/lib/components';
 
 import { hyphenate } from '../lib/utils';
 
@@ -15,31 +15,33 @@ export function ProjectCard(props: Readonly<ProjectCardProps>): JSX.Element {
   const hyphenatedSlug = hyphenate(project.slug);
 
   return (
-    <Card
-      id={hyphenatedSlug}
-      aria-labelledby={`${hyphenatedSlug}-title`}
-      class="flex gap-4 border border-neutral-7"
-      {...rest}
-    >
-      <Show when={project.data.logo}>
-        {logo => <img src={logo()} alt={`${project.data.title} logo`} class="h-16 w-16 rounded-lg" />}
-      </Show>
-      <div>
-        <Typography.Heading id={`${hyphenatedSlug}-title`} size="xsmall" hideAnchor>
-          {project.data.title}
-          <Switch fallback={null}>
-            {/* TODO: replace with some kind of tag/badge component */}
-            <Match when={project.data.status === 'wip'}>
-              <Typography.Text variant="subdued" class="ml-2 text-warning-10">
-                ({project.data.status})
-              </Typography.Text>
-            </Match>
-          </Switch>
-        </Typography.Heading>
-        <Typography.Paragraph size="medium" variant="subdued" class="mt-2">
-          {project.data.description}
-        </Typography.Paragraph>
-      </div>
-    </Card>
+    <Anchor noStyle href={`/project/${project.slug}`} class="group">
+      <Card
+        id={hyphenatedSlug}
+        aria-labelledby={`${hyphenatedSlug}-title`}
+        class={'flex gap-4 group-hover:bg-neutral-5'}
+        {...rest}
+      >
+        <Show when={project.data.logo}>
+          {logo => <img src={logo()} alt={`${project.data.title} logo`} class="h-16 w-16 rounded-lg" />}
+        </Show>
+        <div>
+          <Typography.Heading id={`${hyphenatedSlug}-title`} size="xsmall" hideAnchor>
+            {project.data.title}
+            <Switch fallback={null}>
+              {/* TODO: replace with some kind of tag/badge component */}
+              <Match when={project.data.status === 'wip'}>
+                <Typography.Text variant="subdued" class="ml-2 text-warning-10">
+                  ({project.data.status})
+                </Typography.Text>
+              </Match>
+            </Switch>
+          </Typography.Heading>
+          <Typography.Paragraph size="medium" variant="subdued" class="mt-2">
+            {project.data.description}
+          </Typography.Paragraph>
+        </div>
+      </Card>
+    </Anchor>
   );
 }
