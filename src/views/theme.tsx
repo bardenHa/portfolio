c767c42e590816592e478aef5b8b3535142b4079
harry <bardenha@gmail.com>
2023-10-08 20:48:50 +0100

    fix: theme toggle for mobile

--- src/views/theme.tsx
+++ src/views/theme.tsx
@@ -4,6 +4,29 @@ import { cx } from 'class-variance-authority';
 import { IconButton } from '@/lib/components';
 import { Moon } from '@/lib/icons';
 
+type Theme = 'light' | 'dark';
+
+const DEFAULT_THEME: Theme = 'light';
+
+declare global {
+  interface Window {
+    theme?: {
+      initial: () => Theme;
+      current: () => Theme;
+      set: (theme: Theme) => Theme;
+    };
+  }
+}
+
+function onThemeToggle(): Theme {
+  if (window.theme) {
+    const theme = window.theme.current();
+    return window.theme.set(theme === 'dark' ? 'light' : 'dark');
+  }
+
+  return DEFAULT_THEME;
+}
+
 export function ColorModeSwitcher(props: Readonly<{ class?: string }>): JSX.Element {
   return (
     <IconButton
@@ -15,6 +38,7 @@ export function ColorModeSwitcher(props: Readonly<{ class?: string }>): JSX.Elem
       size={'md'}
       variant={'tertiary'}
       class={cx('h-12 w-12', props.class)}
+      onclick={onThemeToggle}
     />
   );
 }
